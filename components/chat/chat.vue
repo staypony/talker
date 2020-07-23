24fe4fb681e7da1531c327e4d4f0fdcc8a12f9ef
Artem <arediveev@gmail.com>
2020-07-23 18:47:09 +0300

    feat: create chat component

--- components/chat/chat.vue
+++ components/chat/chat.vue
@@ -3,7 +3,36 @@
   <div class="chat">
     <el-upload-file />
     <div class="chat__messages">
-      <el-message />
+      <el-message message="Hello, world!" timestamp="Сегодня, в 19:30" username="Username" />
+      <el-message
+        message="
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat."
+        timestamp="Сегодня, в 19:30"
+        username="Username"
+      />
+      <div class="message">
+        <img src="~/assets/images/user-avatar2.jpg" alt="User Avatar" class="message__avatar" />
+        <div class="message__content">
+          <div class="message__header">
+            <router-link to="/" class="message__author-name link_no_styles">Username</router-link>
+            <time class="message__timestamp">Сегодня, в 19:30</time>
+          </div>
+          <div class="message__body">
+            <span class="message__text">
+              Lorem ipsum dolor sit amet
+              <img src="~assets/emoji/grinning.svg" class="message__emoji" alt="emoji" />
+              <img src="~assets/emoji/smile.svg" class="message__emoji" alt="emoji" />
+              consectetur adipiscing elit
+              <img src="~assets/emoji/sunglasses.svg" class="message__emoji" alt="emoji" />
+              sed do eiusmod tempor incididunt ut labore
+              <img src="~assets/emoji/sunglasses.svg" class="message__emoji" alt="emoji" />
+              asfsf
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <!-- end .chat-->
---
+++ components/message/message.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- begin .message-->
+  <div class="message">
+    <img src="~/assets/images/user-avatar2.jpg" alt="User Avatar" class="message__avatar" />
+    <div class="message__content">
+      <div class="message__header">
+        <router-link to="/" class="message__author-name link_no_styles">{{ username }}</router-link>
+        <time class="message__timestamp">{{ timestamp }}</time>
+      </div>
+      <div class="message__body">
+        <span class="message__text">{{ message }}</span>
+      </div>
+    </div>
+  </div>
+  <!-- end .message-->
+</template>
+
+<style lang="stylus" src="./message.styl" />
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'el-message',
+})
+export default class Message extends Vue {
+  @Prop({ type: String }) message!: string;
+  @Prop({ type: String }) username!: string;
+  @Prop({ type: String }) timestamp!: string;
+}
+</script>
