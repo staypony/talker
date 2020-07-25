8aaf3e8f2a616709b6f6e45ca075b0b60eb7afb0
Artem <arediveev@gmail.com>
2020-07-25 22:38:18 +0300

    feat: create users-list component

--- components/aside-panel/aside-panel.vue
+++ components/aside-panel/aside-panel.vue
@@ -1,6 +1,19 @@
 <template>
   <!-- begin .aside-panel-->
-  <div class="aside-panel" />
+  <div class="aside-panel">
+    <section class="aside-panel__section">
+      <h2 class="aside-panel__section-title">В сети – 10</h2>
+      <div class="aside-panel__section-body">
+        <el-users-list />
+      </div>
+    </section>
+    <section class="aside-panel__section">
+      <h2 class="aside-panel__section-title">Не сети – 10</h2>
+      <div class="aside-panel__section-body">
+        <el-users-list />
+      </div>
+    </section>
+  </div>
   <!-- end .aside-panel-->
 </template>
 
@@ -8,9 +21,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import ElUsersList from '~/components/users-list/users-list.vue';
 
 @Component({
   name: 'el-aside-panel',
+  components: { ElUsersList },
 })
 export default class AsidePanel extends Vue {}
 </script>
---
+++ components/users-list/users-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- begin .users-list-->
+  <ul class="users-list">
+    <li v-for="user in users" :key="user.i" class="users-list__item">
+      <img :src="user.avatar" alt="avatar" class="users-list__avatar" />
+      <div class="users-list__info">
+        <h3 class="users-list__username">{{ user.name }}</h3>
+        <p v-if="user.status" class="users-list__status">{{ user.status }}</p>
+      </div>
+    </li>
+  </ul>
+  <!-- end .users-list-->
+</template>
+
+<style lang="stylus" src="./users-list.styl" />
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'el-users-list',
+})
+export default class UsersList extends Vue {
+  users = [
+    {
+      avatar: '/images/user-avatar2.jpg',
+      name: 'IntelRug',
+      status: 'Играет в Webstorm',
+    },
+    {
+      avatar: '/images/user-avatar.png',
+      name: 'TrenLok',
+    },
+  ];
+}
+</script>
