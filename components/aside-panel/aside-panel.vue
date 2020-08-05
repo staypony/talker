cbf2340918b95c517581ea88ffd4e22da7a1017a
Artem <arediveev@gmail.com>
2020-08-05 18:55:04 +0300

    feat: add a mobile version for the aside-panel

--- components/aside-panel/aside-panel.vue
+++ components/aside-panel/aside-panel.vue
@@ -1,7 +1,70 @@
 <template>
   <!-- begin .aside-panel-->
-  <div class="aside-panel">
+  <div class="aside-panel" :class="{ 'aside-panel_type_open': openMenu }">
+    <div class="aside-panel__open-button" @click="openMenu = !openMenu">
+      <svg-icon name="apps" class="aside-panel__open-icon" />
+    </div>
     <vuescroll>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">В сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">Не сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">В сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">Не сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">В сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">Не сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">В сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">Не сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">В сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
+      <section class="aside-panel__section">
+        <h2 class="aside-panel__section-title">Не сети – 10</h2>
+        <div class="aside-panel__section-body">
+          <el-users-list />
+        </div>
+      </section>
       <section class="aside-panel__section">
         <h2 class="aside-panel__section-title">В сети – 10</h2>
         <div class="aside-panel__section-body">
@@ -30,5 +93,7 @@ import ElUsersList from '~/components/users-list/users-list.vue';
   name: 'el-aside-panel',
   components: { ElUsersList, vuescroll },
 })
-export default class AsidePanel extends Vue {}
+export default class AsidePanel extends Vue {
+  openMenu: boolean = false;
+}
 </script>
